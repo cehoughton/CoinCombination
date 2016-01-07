e3bbed3f7b2b86ea24534677f1f0039b3aca3240
Chris Houghton and Illia Kuchko <ikuchko@gmail.com>
2016-01-07 14:54:36 -0800

    pass integration tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,3 +5,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+  @ClassRule
+    public static ServerRule server = new ServerRule();
+
+  @Test
+    public void rootTest() {
+        goTo("http://localhost:4567/");
+        assertThat(pageSource()).contains("Do you need");
+    }
+
+  @Test
+    public void isALeapYear() {
+    goTo("http://localhost:4567");
+    fill("#sender").with("47");
+    submit(".btn");
+    assertThat(pageSource()).contains("sdfjhsdjkfjs");
+  }
+}
